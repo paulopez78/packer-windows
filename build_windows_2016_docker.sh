eb80447e82493ef04f87a6f74b39fe140343d465
paulopez78 <paulopez78@gmail.com>
2016-10-02 14:52:22 +0200

    use virtualbox iso in the local directory

--- build_windows_2016_docker.sh
+++ build_windows_2016_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-packer build --only=vmware-iso --var iso_url=~/packer_cache/14393.0.160715-1616.RS1_RELEASE_SERVER_EVAL_X64FRE_EN-US.ISO windows_2016_docker.json
+packer build --only=virtualbox-iso --var iso_url=~/projects/packer-windows/iso/14393.0.160715-1616.RS1_RELEASE_SERVER_EVAL_X64FRE_EN-US.ISO windows_2016_docker_core.json
